d921ce225ad13c4ca3ec2e9a1593fa9f47c6dbd1
Praney Behl <praneybehl@gmail.com>
2019-10-20 18:47:56 +1100

    feat(components): add ResponsiveImage component

---
+++ src/components/ResponsiveImage/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import PropTypes from 'prop-types';
+import {display, size, width, height, compose} from 'styled-system';
+import {propType} from '@styled-system/prop-types';
+
+// ResponsiveImage styles
+const ImageStyled = styled.img`
+	max-width: 100%;
+	height: auto;
+	${compose(display, size, width, height)}
+`;
+
+// ResponsiveImage component
+const ResponsiveImage = ({src1x, src2x, width}) => (
+	<ImageStyled
+		src={src1x}
+		srcSet={`${src1x} 1x, ${src2x} 2x`}
+	/>
+);
+
+// ResponsiveImage prop types
+ResponsiveImage.propTypes = {
+	src1x: PropTypes.string,
+	src2x: PropTypes.string,
+	display: propType,
+	size: propType,
+	width: propType,
+	height: propType
+};
+
+export default ResponsiveImage;
