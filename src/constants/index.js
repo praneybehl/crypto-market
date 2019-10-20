3d73a8762cb7db4ef84a9dc98c99b3bcf7808ce5
Praney Behl <praneybehl@gmail.com>
2019-10-20 19:42:48 +1100

    refactor: refactor constants data for pages

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+export const constants = {
+	// Currencies for Home page
+	'BTC': {
+		'currencyName': 'Bitcoin',
+		'image': {
+			'src1x': '/images/icon-bitcoin.png',
+			'src2x': '/images/icon-bitcoin@2x.png',
+		}
+	},
+	'LTC': {
+		'currencyName': 'Litecoin',
+		'image': {
+			'src1x': '/images/icon-litecoin.png',
+			'src2x': '/images/icon-litecoin@2x.png',
+		}
+	},
+	'ETH': {
+		'currencyName': 'Ethereum',
+		'image': {
+			'src1x': '/images/icon-ethereum.png',
+			'src2x': '/images/icon-ethereum@2x.png',
+		}
+	},
+	'XRP': {
+		'currencyName': 'Ripple',
+		'image': {
+			'src1x': '/images/icon-ripple.png',
+			'src2x': '/images/icon-ripple@2x.png',
+		}
+	},
+	'ZEC': {
+		'currencyName': 'Zcash',
+		'image': {
+			'src1x': '/images/icon-zcash@1x.png',
+			'src2x': '/images/icon-zcash@2x.png',
+		}
+	},
+
+	// Trending UP and DOWN icons form History page
+	'T_UP': {
+		'src1x': '/images/icon-trend-up.png',
+		'src2x': '/images/icon-trend-up@2x.png',
+	},
+	'T_DOWN': {
+		'src1x': '/images/icon-trend-down.png',
+		'src2x': '/images/icon-trend-down@2x.png',
+	},
+};
