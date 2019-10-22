50b17b2dcb35c05645e7d97097635cf3ea0be0e4
Praney Behl <praneybehl@gmail.com>
2019-10-22 20:57:22 +1100

    feat(components): add Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import RLoader from 'react-loader-spinner';
+import styled from 'styled-components';
+
+const LoaderContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100vw;
+	height: 200px;
+`;
+const Loader = () => {
+	return (
+		<LoaderContainer>
+			<RLoader
+				type="Bars"
+				color="#1058a1"
+				height={40}
+				width={40}
+			/>
+		</LoaderContainer>
+	)
+};
+
+export default Loader;
