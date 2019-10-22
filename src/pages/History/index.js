e10277cf945090cd62c4a9ee61f9cc91e8d7ddae
Praney Behl <praneybehl@gmail.com>
2019-10-22 22:40:18 +1100

    feat(pages): add History page

--- src/pages/History/index.js
+++ src/pages/History/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
+import Container from '../../components/Container';
+import DefaultLayout from '../../layouts/default';
+import {splitValueWithDelimiter} from '../../utils';
+import {getCoinHistory} from '../../api';
+import HistoryTable from "../../components/HistoryTable";
+
+const getPageTitle = (currencyPair) =>
+	`${splitValueWithDelimiter(currencyPair, 3, '/')} Trade History`;
 
 const HistoryPage = ({currencyPair}) => {
-    return <div>{currencyPair}</div>;
+	const headerTitle =  getPageTitle(currencyPair);
+	const [historyData, setHistoryData] = useState([]);
+	useEffect(() => {
+		getCoinHistory(currencyPair, setHistoryData);
+	}, [currencyPair]);
+
+    return <DefaultLayout headerProps={{title: headerTitle}}>
+		<Container>
+			<HistoryTable priceList={historyData}/>
+		</Container>
+	</DefaultLayout>;
 };
 
 HistoryPage.propTypes = {
